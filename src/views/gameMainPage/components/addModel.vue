5128b89e7c0b2aa553ad46ba8e8eca385509c911
whq <710721802@qq.com>
2022-06-18 14:10:04 +0800

    更滑图片资源

--- src/views/gameMainPage/components/addModel.vue
+++ src/views/gameMainPage/components/addModel.vue
@@ -1,7 +1,7 @@
 <!--
  * @Date: 2022-06-16 22:11:33
  * @LastEditors: whq 710721802@qq.com
- * @LastEditTime: 2022-06-18 00:29:37
+ * @LastEditTime: 2022-06-18 14:00:00
  * @FilePath: \zb\src\views\gameMainPage\components\addModel.vue
 -->
 <template>
@@ -10,6 +10,35 @@
       <TopUserinfo></TopUserinfo>
       <div class="game-box">
         <van-button class="okBtn" type="primary" @click="hideModal">确认</van-button>
+        <div class="cjjs">
+          创建角色
+        </div>
+        <div class="nameAndAgeBox">
+          <label for="name">角色昵称 </label>
+          <input
+            type="text"
+            name="name"
+          >
+          <label for="age">角色年龄 </label>
+          <input
+            type="number"
+            value="10"
+            name="age"
+          >
+        </div>
+        <div class="chooseRoleStyle">
+          <div class="title">选择角色样式</div>
+          <ul class="roleListBox">
+            <li class="roleItem">
+              <div class="imgBox">
+                <img src="@/assets/game/center.png" alt="">
+              </div>
+              <div class="name">
+                暮年男
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </van-overlay>
@@ -58,6 +87,7 @@ $bk_blur: #031428;
     width: 966px;
     height: 622px;
     margin: 0 auto;
+    text-align: left;
     .okBtn {
       width: 100px;
       height: 40px;
@@ -66,6 +96,49 @@ $bk_blur: #031428;
       right: 24px;
       top: 32px;
       background: linear-gradient(#EFCB49, #DA7700);
+    }
+    .cjjs {
+      padding: 33px 0 29px 25px;
+      font-size: 40px;
+      font-style: italic;
+      font-weight: bold;
+    }
+    .nameAndAgeBox {
+      margin-left: 25px;
+      input {
+        color: #333;
+        width: 162px;
+        height: 38px;
+        margin-right: 28px;
+        border-radius: 6px;
+        padding: 0 10px;
+      }
+    }
+    .chooseRoleStyle {
+      padding: 36px 0 26px 25px;
+      .title {
+        font-size: 17px;
+        width: 200px;
+        background: -webkit-linear-gradient(left, rgba(0, 114, 255, 0.68), rgba(45, 124, 255, 0));
+        padding: 6px 0 6px 10px;
+      }
+      .roleListBox {
+        .roleItem {
+          width: 92px;
+          height: 92px;
+          background-color: #fff;
+          .imgBox {
+            height: 75px;
+          }
+          .name {
+            height: 17px;
+            line-height: 17px;
+            font-size: 12px;
+            text-align: center;
+            background-color: #0A305F;
+          }
+        }
+      }
     }
   }
 }
