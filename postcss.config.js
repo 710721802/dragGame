6d772ee52c7de09be29367e90a865628811321b2
whq <710721802@qq.com>
2022-07-17 18:31:05 +0800

    npm install postcss-pxtorem@5.1 lib-flexible

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2022-07-16 12:56:45
  * @LastEditors: whq 710721802@qq.com
- * @LastEditTime: 2022-07-16 12:57:03
+ * @LastEditTime: 2022-07-17 18:26:13
  * @FilePath: \zb\postcss.config.js
  */
 // postcss.config.js
