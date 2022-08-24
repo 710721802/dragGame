062f99dbe7fc4b60f7c45efbf6ffa608e86bc614
whq <710721802@qq.com>
2022-08-24 20:46:30 +0800

    封装

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 /*
  * @Date: 2022-08-10 23:54:39
  * @LastEditors: whq 710721802@qq.com
- * @LastEditTime: 2022-08-24 20:36:33
+ * @LastEditTime: 2022-08-24 20:46:20
  * @FilePath: \zb\src\utils\request.js
  */
 import axios from 'axios'
@@ -20,6 +20,7 @@ request.interceptors.request.use(function (config) {
 }, function (error) {
   return Promise.reject(error)
 })
+
 /**
   * get请求
   * @param {string} url 请求地址
@@ -52,7 +53,157 @@ request.interceptors.request.use(function (config) {
     return res
   }
 }
+/**
+  * post请求
+  * @param {string} url 请求地址
+  * @param {object} data 请求参数
+  * @param {object} config 请求配置，包含headers等
+  * @param {boolean} automaticJudgment 是否自行判断返回值并处理
+  * @return {object} 接口返回信息
+  */
+ const post = async (url, data, config = {}, automaticJudgment = true) => {
+  const {
+    data: res
+  } = await request({
+    ...config,
+    url,
+    method: 'post',
+    data
+  })
+
+  if (!automaticJudgment) {
+    return res
+  }
+
+  if (res && res.status === 0) {
+    if (res.result) {
+      return res.result
+    }
+    return res
+  } else {
+    return res
+  }
+}
+
+/**
+* put请求
+* @param {string} url 请求地址
+* @param {object} data 请求参数
+* @param {object} config 请求配置，包含headers等
+* @param {boolean} automaticJudgment 是否自行判断返回值并处理
+* @return {object} 接口返回信息
+*/
+const put = async (url, data, config = {}, automaticJudgment = true) => {
+  const {
+    data: res
+  } = await request.put(url, data, config)
+
+  if (!automaticJudgment) {
+    return res
+  }
+
+  if (res && res.status === 0) {
+    if (res.result) {
+      return res.result
+    }
+    return res
+  } else {
+    return res
+  }
+}
+
+/**
+* patch请求
+* @param {string} url 请求地址
+* @param {object} data 请求参数
+* @param {object} config 请求配置，包含headers等
+* @param {boolean} automaticJudgment 是否自行判断返回值并处理
+* @return {object} 接口返回信息
+*/
+const patch = async (url, data, config = {}, automaticJudgment = true) => {
+  const {
+    data: res
+  } = await request.patch(url, data, config)
+
+  if (!automaticJudgment) {
+    return res
+  }
+
+  if (res && res.status === 0) {
+    if (res.result) {
+      return res.result
+    }
+    return res
+  } else {
+    return res
+  }
+}
+
+/**
+* remove请求
+* @param {string} url 请求地址
+* @param {object} data 请求参数
+* @param {object} config 请求配置，包含headers等
+* @param {boolean} automaticJudgment 是否自行判断返回值并处理
+* @return {object} 接口返回信息
+*/
+const remove = async (url, data, config = {}, automaticJudgment = true) => {
+  const {
+    data: res
+  } = await request.delete(url, {
+    ...config,
+    params: data
+  })
+
+  if (!automaticJudgment) {
+    return res
+  }
+
+  if (res && res.status === 0) {
+    if (res.result) {
+      return res.result
+    }
+    return res
+  } else {
+    return res
+  }
+}
+
+/**
+* remove请求
+* @param {string} url 请求地址
+* @param {object} data 请求参数
+* @param {object} config 请求配置，包含headers等
+* @param {boolean} automaticJudgment 是否自行判断返回值并处理
+* @return {object} 接口返回信息
+*/
+ const methodRemove = async (url, data, config = {}, automaticJudgment = true) => {
+  const {
+    data: res
+  } = await request.delete(url, {
+    ...config,
+    data
+  })
+
+  if (!automaticJudgment) {
+    return res
+  }
+
+  if (res && res.status === 0) {
+    if (res.result) {
+      return res.result
+    }
+    return res
+  } else {
+    return res
+  }
+}
 
 export default {
   get,
+  post,
+  put,
+  patch,
+  remove,
+  methodRemove,
 }
